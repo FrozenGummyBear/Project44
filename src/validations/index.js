f1516f556183bab58563ec9503bb7bab967f4e56
Henok Zelalem <z.w.henok@gmail.com>
2022-10-22 11:07:10 +0300

    update

--- src/validations/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.authValidation = require("./auth.validation");
-module.exports.userValidation = require("./user.validate");
-
-///////////////////////////////////////////////////////////
-
-module.exports.gameValidation = require("./game.validation");
-module.exports.recommendationValidation = require("./recommendation.validation");
